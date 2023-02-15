bdd2f7ef91b06724c03bd3cdf73c3377b196b7b1
adi bangkit <adibangkai@gmail.com>
2023-02-15 09:04:59 +0700

    fix notif

--- static/main.js
+++ static/main.js
@@ -1,73 +1,93 @@
-const form = document.querySelector('.periksa')
-const formTeks = document.querySelector('.periksa-teks')
-const show = document.querySelector('.show')
-const food = document.querySelector('.food')
-const other = document.querySelector('.other')
-const loading = document.querySelector('.loading')
-const image = document.querySelector('.image')
-const ocr = document.querySelector('.ocr')
-const error = document.querySelector('.error')
+const form = document.querySelector(".periksa");
+const formTeks = document.querySelector(".periksa-teks");
+const show = document.querySelector(".show");
+const notif = document.querySelector(".notif");
+const food = document.querySelector(".food");
+const other = document.querySelector(".other");
+const loading = document.querySelector(".loading");
+const image = document.querySelector(".image");
+const ocr = document.querySelector(".ocr");
+const error = document.querySelector(".error");
 
-const predict = new Predict()
+const predict = new Predict();
 
+const validateFileType = () => {
+  var fileName = document.getElementById("fileName").value;
+  var idxDot = fileName.lastIndexOf(".") + 1;
+  var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+  console.log(fileName);
+  if (extFile == "heic" || extFile == "pdf" || extFile == "docx") {
+    console.log("catch");
+
+    notif.classList.remove("hide");
+  } else if (extFile == "jpg" || extFile == "jpeg" || extFile == "png") {
+    form.addEventListener("change", (e) => {
+      e.preventDefault();
+      show.classList.add("hide");
+      let dataFoto = form.file.value;
+      const formData = new FormData(form);
+      formData.append("rawtext", dataFoto);
+      loading.classList.remove("hide");
+      error.classList.add("hide");
+
+      console.log("run");
+      predict
+        .getEntities(formData)
+        .then((data) => updateUI(data))
+        .catch((err) => console.log(err));
+    });
+  } else {
+    console.log("catch error dong");
+    notif.classList.remove("hide");
+  }
+};
 
 const updateUI = (data) => {
-  const { entitiesResult } = data
-  console.log(entitiesResult)
-  food.innerHTML = ''
-  other.innerHTML = ''
-  ocr.innerHTML = ''
-  image.innerHTML = ''
-  loading.classList.add('hide')
-  show.classList.remove('hide')
+  const { entitiesResult } = data;
+  error.classList.add("hide");
+
+  food.innerHTML = "";
+  other.innerHTML = "";
+  ocr.innerHTML = "";
+  image.innerHTML = "";
+  loading.classList.add("hide");
+  show.classList.remove("hide");
+  notif.classList.add("hide");
+
   if (entitiesResult.status === 200) {
     // image.innerHTML += `<img src="https://554f-114-124-175-166.ngrok.io/static/uploads/${entitiesResult.filename}" class='image'>`
-    image.innerHTML += `<img src="/static/uploads/${entitiesResult.filename}" class='image'>`
+    image.innerHTML += `<img src="/static/uploads/${entitiesResult.filename}" class='image'>`;
     // ocr.innerHTML += `${entitiesResult.result}`
     entitiesResult.detected_entities.forEach((detected_entities) => {
       food.innerHTML += `
               <a href="https://www.google.com/search?q=is+${detected_entities.name}+halal" target="_blank"><span class="entitas ${detected_entities.entities}">${detected_entities.name}</span></a>
-          `
-    })
+          `;
+    });
     entitiesResult.other_entities.forEach((other_entities) => {
       other.innerHTML += `
               <span class="entitas">${other_entities.name} |<strong> ${other_entities.entities}</strong></span>
-          `
-    })
+          `;
+    });
+  } else {
+    error.classList.remove("hide");
   }
-}
+};
 
-form.addEventListener('change', (e) => {
-  e.preventDefault()
-  show.classList.add('hide')
+formTeks.addEventListener("submit", (e) => {
+  e.preventDefault();
+  show.classList.add("hide");
 
-  let dataFoto = form.file.value
-  console.log('data', dataFoto)
-  const formData = new FormData(form)
-  formData.append('rawtext', dataFoto)
-  loading.classList.remove('hide')
+  let dataKomposisi = formTeks.komposisi.value;
 
+  const formData = new FormData(formTeks);
+  formData.append("rawtext", dataKomposisi);
+  loading.classList.remove("hide");
+
+  food.innerHTML = "";
+  other.innerHTML = "";
+  image.innerHTML = "";
   predict
     .getEntities(formData)
     .then((data) => updateUI(data))
-    .catch((err) => console.log(err))
-})
-
-formTeks.addEventListener('submit', (e) => {
-  e.preventDefault()
-  show.classList.add('hide')
-
-  let dataKomposisi = formTeks.komposisi.value
-  console.log('data', dataKomposisi)
-  const formData = new FormData(formTeks)
-  formData.append('rawtext', dataKomposisi)
-  loading.classList.remove('hide')
-
-  food.innerHTML = ''
-  other.innerHTML = ''
-  image.innerHTML = ''
-  predict
-    .getEntities(formData)
-    .then((data) => updateUI(data))
-    .catch((err) => console.log(err))
-})
+    .catch((err) => console.log(err));
+});
--- static/predict.js
+++ static/predict.js
@@ -1,17 +1,15 @@
-
 class Predict {
   constructor() {
-    // this.url = 'https://554f-114-124-175-166.ngrok.io/api/extract'
-    this.url = '/api/extract'
+    this.url = "/api/extract";
   }
 
   async getEntities(formData) {
     const response = await fetch(this.url, {
-      method: 'POST',
-      body: formData
-    })
-    const entitiesResult = await response.json()
+      method: "POST",
+      body: formData,
+    });
+    const entitiesResult = await response.json();
 
-    return { entitiesResult }
+    return { entitiesResult };
   }
 }
